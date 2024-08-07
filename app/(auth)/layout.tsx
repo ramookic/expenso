44e82246987097d091da763c783dbf6adaf5cfba
Ramo Okić <130864796+ramookic@users.noreply.github.com>
2024-08-07 18:41:49 +0200

    feat(auth): redirect authenticated users to dashboard from login and register pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,19 @@
 import Logo from "@/components/ui/logo";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const supabase = createClient();
+  const user = await supabase.auth.getUser();
+
+  if (user) {
+    redirect("/");
+  }
+
   return (
     <div className="h-screen flex flex-col gap-2 justify-center items-center">
       <Logo />
