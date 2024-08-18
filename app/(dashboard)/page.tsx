68b46c377724eff555935f0e150014edeac43b31
Ramo Okić <130864796+ramookic@users.noreply.github.com>
2024-08-18 15:34:03 +0200

    feat(dashboard): add date range picker

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,4 @@
+import DateRangePicker from "@/components/dashboard/date-range-picker";
 import { format } from "date-fns";
 import { enGB } from "date-fns/locale/en-GB";
 
@@ -9,9 +10,12 @@ const Page = () => {
   const formattedDate = format(new Date(), "PPP", { locale: enGB });
   return (
     <div>
-      <div>
-        <p className="text-sm text-zinc-500">{formattedDate}</p>
-        <h1 className="text-2xl font-semibold">Dashboard</h1>
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="text-sm text-zinc-500">{formattedDate}</p>
+          <h1 className="text-2xl font-semibold">Dashboard</h1>
+        </div>
+        <DateRangePicker />
       </div>
     </div>
   );
